cc3f95d79b2c7ea0aeb4f4a03b9ee1050848dc87
Victor Alcaraz <vctralcaraz@gmail.com>
2024-02-28 21:00:19 -0800

    day 1 complete

--- public/index.js
+++ public/index.js
@@ -1,1 +1,28 @@
 // Your code here
+
+window.onload = () => {
+
+    const h1 = document.createElement('h1');
+    const img = document.createElement('img');
+
+    h1.innerHTML = 'Kitten Pic';
+    getRandomCatPicture().then(data => {
+        img.setAttribute('src', data[0].url);
+    }).catch(error => {
+        img.setAttribute('src', 'https://placehold.co/600x400/png');
+    });
+
+    document.body.appendChild(h1);
+    document.body.appendChild(img);
+}
+
+const getRandomCatPicture = () => {
+    const apiUrl = 'https://api.thecatapi.com/v1/images/search';
+    return new Promise((resolve, reject) => {
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                resolve(data);
+            });
+    });
+}
